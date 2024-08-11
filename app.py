6f11c5c7a1918455fe81d3d217f17618b3ee5064
JadenLeung <jaden.leung@gmail.com>
2024-08-11 00:58:27 -0400

    Added additional parameter for testing

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import openai
 
 openai.api_key = os.getenv('OPENAIKEY')
 
-def gpt(message):
+def gpt(message, mod):
     messages = [ {"role": "system", "content":  
                 # f"It's your turn as black. Which move will come next? (FEN): {message}'. Answer strictly in the form [Piece name]: [Old square]->[New square]"} ] 
                 message} ] 
 
     chat = openai.ChatCompletion.create( 
-        model="gpt-4o-mini", messages=messages 
+       # model="gpt-4o-mini", messages=messages
+        model = mod, messages = messages
     ) 
     reply = chat.choices[0].message.content 
     return reply
@@ -29,11 +30,12 @@ def submit():
     data = request.get_json()
 
     mes = data.get('message')
+    mod = data.get('model')
 
     gptresponse = "Not a valid FEN"
     
     if (mes.count('/') >= 7):
-        gptresponse = gpt(mes)
+        gptresponse = gpt(mes, mod)
 
     return gptresponse, 200
 
