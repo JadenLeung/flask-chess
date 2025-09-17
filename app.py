d458827a0e4a7b11d17a51104d19993a86be1791
JadenLeung <jaden.leung@gmail.com>
2025-09-17 15:52:24 -0400

    upgraded model

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def gpt(message, mod, color, prev):
     return reply
 
 def gemini(message, color, prev):
-    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    model = genai.GenerativeModel(model_name="gemini-2.5-flash-lite")
     
     response = model.generate_content([
         f"It's your turn as {color}. (Previous Moves): {prev}, (FEN): {message}. Without giving explanations, please state the best 5 moves for {color}, each in a new line."],
